f64cb580ad6d993bf2b11392b9d724efb80e34db
amoolyareddy <g.amoolyareddy@gmail.com>
2017-03-28 17:10:26 -0500

    recently changed

---
+++ test/test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine("hello welcome to visual studio");
+
+        }
+    }
+}
